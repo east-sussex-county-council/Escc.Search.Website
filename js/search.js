e9d031f9dc847d255fa580803d6dc0f52c484056
Rick Mason <richard.mason@eastsussex.gov.uk>
2015-07-27 11:00:48 +0100

    Update event tracking to Universal Analytics

--- js/search.js
+++ js/search.js
@@ -4,13 +4,13 @@
     var resultOrPagingClicked = false;
     $(window).unload(function () {
         if (!resultOrPagingClicked) {
-            if (typeof (_gaq) != 'undefined') _gaq.push(['_trackEvent', 'search-results', "none clicked"]);
+            if (typeof(ga) !== 'undefined') ga('send', 'event', 'search-results', "none clicked");
         }
     });
 
     $(function () {
         // Ignore any clicks on paging nav. This script will run again on next page.
-        $(".pagingPages a").click(function () { resultOrPagingClicked = true; })
+        $(".pagingPages a").click(function() { resultOrPagingClicked = true; });
 
         // For any click on a search result...
         $(".article dt a").click(function () {
@@ -26,7 +26,7 @@
             positionClicked = (((pageNumber - 1) * 20) + positionClicked);
 
             // Track which result was clicked, and the file format as we suspect that may affect it
-            if (typeof (_gaq) != 'undefined') _gaq.push(['_trackEvent', 'search-results', positionClicked.toString(), this.className]);
+            if (typeof(ga) !== 'undefined') ga('send', 'event', 'search-results', positionClicked.toString(), this.className);
 
             // Don't track this a second time at onunload
             resultOrPagingClicked = true;
